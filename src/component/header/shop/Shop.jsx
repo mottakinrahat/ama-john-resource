88ef4ef3fa2d52abbfdb691359f53505274860e4
mottakinrahat <rahatchowhdury661@gmail.com>
2023-03-29 13:58:14 +0600

    retrive

--- src/component/header/shop/Shop.jsx
+++ src/component/header/shop/Shop.jsx
@@ -18,8 +18,21 @@ const Shop = () => {
 
 useEffect(()=>{
     const storedCart=getShoppingCart();
-    console.log(storedCart);
-},[])
+    const savedCart=[];
+    //step1 get Id
+    for(const id in storedCart){
+    const addedProduct=products.find(product=>product.id === id)
+   if(addedProduct){
+     
+    const quantity=storedCart[id];
+    addedProduct.quantity=quantity;
+    savedCart.push(addedProduct);
+   }
+   
+    }
+    setCart(savedCart);
+   
+},[products]);
 
 
    const handleAddCart=(product)=>{
